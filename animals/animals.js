c074529406fda608f851580377b23a4d99044fbe
Chad Stabler <stablerpsn@gmail.com>
2022-05-09 14:29:25 -0500

    animals displaying correctly in new page, functions and parameters used correctly

--- animals/animals.js
+++ animals/animals.js
@@ -1,6 +1,18 @@
-import { renderListItem } from '../render-utils';
-import { findById } from '../utils';
+import { findById } from '../utils.js';
+import { animals } from '../data.js';
 
+const params = new URLSearchParams(window.location.search);
+const animalName = document.getElementById('animal-name');
+const imageEl = document.getElementById('animal-image');
+const name = document.getElementById('name');
+const type = document.getElementById('type');
+const phrase = document.getElementById('phrase');
 
+const animal = findById(params.get('id'), animals);
 
+animalName.textContent = animal.name;
+name.textContent = animal.name;
+type.textContent = animal.type;
+phrase.textContent = animal.says;
+imageEl.src = `../assets/${animal.type}.svg`;
 
